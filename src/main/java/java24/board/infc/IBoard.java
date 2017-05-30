a87276f8cb88b5490c73845a32c0bfac9ab3205f
fbrkdals1231 <fbrkdals1231@naver.com>
2017-05-30 16:02:39 +0900

    dasd

--- src/main/java/java24/board/infc/IBoard.java
+++ src/main/java/java24/board/infc/IBoard.java
@@ -13,56 +13,89 @@ public interface IBoard {
     String getBoardName(String boardcd);
     // resultType 메서드명 (parameter type 컬럼명);
     //
+    
     ModelBoard  getBoardOne(String boardcd);
     //
+    
     List<ModelBoard> getBoardList();
-   //
-    List<ModelBoard> getBoardListResultMap();
-    //
-    HashMap getBoardCustomMap();
-    //
-    Integer insertBoard(ModelBoard board);
-    //
-    Integer updateBoard(ModelBoard searchValue, ModelBoard updateValue);
-    //
-    Integer deleteBoard(ModelBoard board);
-    //
-    ModelBoard getBoardSearch(ModelBoard board);
-    //
-    ModelBoard getBoardPaging(ModelBoard searchValue, ModelBoard updateValue);
-    //
-    Integer insertBoardList(java.util.List  list);
-    //
-    Integer getArticleTotalRecord(ModelArticle searchValue, ModelArticle updateValue);
-    //
-    ModelArticle getArticleList(ModelArticle searchValue, ModelArticle updateValue);
-    //
-    ModelArticle getArticle(Integer articleNo);
-    //
-    Integer insertArticle(ModelArticle article);
-    //
-    Integer updateArticle(ModelArticle searchValue, ModelArticle updateValue);
-    //
-    Integer deleteArticle(ModelArticle ModelArticle);
-    //
-    Integer increaseHit(Integer articleNo);
-    // int update(ModelBook wherebook, ModelBook setbook)  ;
-    ModelArticle getNextArticle(ModelArticle searchValue);
-    //parameterType="hashmap" resultType="ModelArticle"
-    ModelArticle getPrevArticle(ModelArticle searchValue);
-    //getPrevArticle" parameterType="hashmap" resultType="ModelArticle"
-
-    //
-
-    //
-
-    //
-
-    //
-
-    //
-
     //
     
+    List<ModelBoard> getBoardListResultMap();
+    //
+    
+    HashMap getBoardCustomMap();
+    //
+    
+    Integer insertBoard(ModelBoard board);
+    //
+    
+    Integer updateBoard(ModelBoard searchValue, ModelBoard updateValue);
+    //
+    
+    Integer deleteBoard(ModelBoard board);
+    //
+    
+    ModelBoard getBoardSearch(ModelBoard board);
+    //
+    
+    ModelBoard getBoardPaging(ModelBoard searchValue, ModelBoard updateValue);
+    //
+    
+    Integer insertBoardList(java.util.List  list);
+    //
+    
+    Integer getArticleTotalRecord(ModelArticle searchValue, ModelArticle updateValue);
+    //
+    
+    ModelArticle getArticleList(ModelArticle searchValue, ModelArticle updateValue);
+    //
+    
+    ModelArticle getArticle(Integer articleNo);
+    //
+    
+    Integer insertArticle(ModelArticle article);
+    //
+    
+    Integer updateArticle(ModelArticle searchValue, ModelArticle updateValue);
+    //
+    
+    Integer deleteArticle(ModelArticle ModelArticle);
+    //
+    
+    Integer increaseHit(Integer articleNo);
+    //
+    
+    ModelArticle getNextArticle(ModelArticle searchValue,ModelArticle updateValue);
+    //
+    
+    ModelArticle getPrevArticle(ModelArticle searchValue);
+    //
+
+    ModelAttachFile getAttachFile();
+    //
+
+    ModelAttachFile getAttachFileList();
+    //
+
+    ModelAttachFile insertAttachFile();
+    //
+
+    ModelAttachFile deleteAttachFile();
+    //
+
+    ModelComments getComment();
+    //
+    
+    ModelComments getCommentList();
+    //
+    
+    ModelComments insertComment();
+    //
+    
+    int updateComment();
+    //
+    
+    ModelComments deleteComment();
+    //
     
 }
