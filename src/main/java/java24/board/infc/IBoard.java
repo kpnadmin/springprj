19061b57f58cad4106996f1d1a2a96b72935bb09
fbrkdals1231 <fbrkdals1231@naver.com>
2017-05-30 16:43:08 +0900

    d

--- src/main/java/java24/board/infc/IBoard.java
+++ src/main/java/java24/board/infc/IBoard.java
@@ -15,72 +15,89 @@ public interface IBoard {
     String getBoardName(String boardcd);
     // resultType 메서드명 (parameter type 컬럼명);
     //
+    
     ModelBoard  getBoardOne(String boardcd);
     //
+    
     List<ModelBoard> getBoardList();
-   //
+    //
+    
     List<ModelBoard> getBoardListResultMap();
     //
+    
     HashMap getBoardCustomMap();
     //
+    
     Integer insertBoard(ModelBoard board);
     //
+    
     Integer updateBoard(ModelBoard searchValue, ModelBoard updateValue);
     //
+    
     Integer deleteBoard(ModelBoard board);
     //
+    
     ModelBoard getBoardSearch(ModelBoard board);
     //
+    
     ModelBoard getBoardPaging(ModelBoard searchValue, ModelBoard updateValue);
     //
+    
     Integer insertBoardList(java.util.List  list);
     //
+    
     Integer getArticleTotalRecord(ModelArticle searchValue, ModelArticle updateValue);
     //
+    
     ModelArticle getArticleList(ModelArticle searchValue, ModelArticle updateValue);
     //
+    
     ModelArticle getArticle(Integer articleNo);
     //
+    
     Integer insertArticle(ModelArticle article);
     //
+   
     Integer updateArticle(ModelArticle searchValue, ModelArticle updateValue);
     //
+    
     Integer deleteArticle(ModelArticle ModelArticle);
     //
+    
     Integer increaseHit(Integer articleNo);
-    // int update(ModelBook wherebook, ModelBook setbook)  ;
+    //
+    
     ModelArticle getNextArticle(ModelArticle searchValue, ModelArticle updateValue);
     //
+    
+    ModelArticle getPrevArticle(ModelArticle key,ModelArticle values);
     //
+    
+    ModelAttachfile getAttachFile(int attachFileNo);
     //
-
+    
+    ModelAttachfile getAttachFileList(int articleNo);
     //
-
+    
+    ModelAttachfile insertAttachFile(ModelAttachfile AttachFile);
     //
-
+    
+    
+    ModelAttachfile deleteAttachFile(ModelAttachfile AttachFile );
     //
-
-    //
-
-    //
-
-    //
-
-    //
-    //
-    //
-    Integer getAttachFileList();
-    //
-    Integer deleteAttachFile(ModelAttachfile attachfile);
-    //
+    
     ModelComments getComment(Integer commentNo);
     //
+    
     List<ModelComments> getCommentList(Integer articleNo);
     //
+    
     Integer insertComment(ModelComments comments);
     //
+    
     Integer updateComment(ModelArticle searchValue, ModelArticle updateValue);
     //
+    
     Integer deleteComment(ModelComments comments);
     
 }
